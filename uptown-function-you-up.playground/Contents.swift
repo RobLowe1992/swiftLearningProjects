c5bd32ca5c9bb4b43feafa91d2ee54df267cb81e
Robert <RobLowe1992@yahoo.com>
2016-09-04 14:35:59 -0400

    Updated Uptown Function You Up

--- uptown-function-you-up.playground/Contents.swift
+++ uptown-function-you-up.playground/Contents.swift
@@ -2,23 +2,62 @@
 
 import UIKit
 
-var bankAccount = 550
-var itemAmount = 325
+func funkA(){
+    print("Calling funkB")
+    funkB()
+}
+
+func funkB(){
+    print("Calling funkC")
+    funkC()
+}
+
+func funkC(){
+    print("I am the best function of all")
+}
+funkA()
+
+
+
+var bankAccount = 350.50
+var itemAmount = 320.20
 
 func canPurchase(amount: Double) -> Bool {
     if bankAccount >= amount {
         return true
-}
+    }
         return false
 }
 
-func processPurchase(amount: Double) {
+func processPayment(amt: Double) {
     bankAccount -= amt
-    print("You made a purchase of the amount \(amt)")
-    
-    if canPurchase(itemAmount) {
-        processPurchase(itemAmount)
-    } else {
-        print("Insufficient funds")
-    }
+    print("You have just made a purchase of \(amt)")
 }
+
+if canPurchase(itemAmount){
+    processPayment(itemAmount)
+} else {
+    print("Insufficient Funds")
+}
+
+var name = "sweeny todd"
+
+
+func getUpperVersion(inputStr: String) -> String {
+    return inputStr.uppercaseString
+}
+var nameUpper = getUpperVersion(name)
+
+func declareWinner(playerAScore : Int, playerBScore : Int) {
+    if playerAScore > playerBScore{
+        print("Player A Wins")
+    } else if playerBScore > playerAScore{
+        print("Player B wins")
+    } else {
+        print("The game is at a standstill")
+        
+    }
+        
+    }
+
+    declareWinner(60, playerBScore: 60)
