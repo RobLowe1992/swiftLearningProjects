edb39a9858f2418819fc03ecabd8c9505a707066
Robert <RobLowe1992@yahoo.com>
2016-09-07 11:31:58 -0400

    Application is functioning with minor bugs

--- Tapper/Tapper/ViewController.swift
+++ Tapper/Tapper/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var maxTaps = 0
+    var currentTaps = 0
 
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var textfieldtxt: UITextField!
@@ -17,16 +20,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
     
+    @IBAction func onCoinPress(sender: UIButton!) {
+    
+        currentTaps+=1
+        updateTapsLbl()
+        
+        if isGameOver(){
+            restartGame()
+        }
+    }
+    
     @IBAction func onPlayButtonPressed(sender:UIButton!){
+        
+        if textfieldtxt.text != nil && textfieldtxt.text != "" {
+        
         logoImg.hidden = true
         textfieldtxt.hidden = true
         playBtn.hidden = true
         
         tapsLbl.hidden = false
         tapBtn.hidden = false
+            
+        maxTaps = Int(textfieldtxt.text!)!
+        currentTaps = 0
+            
+        updateTapsLbl()
+    }
+        
     }
     
+    func restartGame() {
+        maxTaps = 0
+        textfieldtxt.text = ""
+        
+        logoImg.hidden = false
+        textfieldtxt.hidden = false
+        playBtn.hidden = false
+        
+        tapsLbl.hidden = true
+        tapBtn.hidden = true
+        
+    }
     
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    func updateTapsLbl() {
+        tapsLbl.text = ("\(currentTaps) Taps")
+    }
 
 
 }
